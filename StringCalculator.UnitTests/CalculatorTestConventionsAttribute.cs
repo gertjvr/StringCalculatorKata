b8e163892e022c07f89afcbde4b81c490cd809c3
Gert Jansen van Rensburg <gertjvr@gmail.com>
2013-09-05 22:29:02 +1000

    Added CalculatorTestConvensionsAttribute

---
+++ StringCalculator.UnitTests/CalculatorTestConventionsAttribute.cs
@@ -0,0 +1,14 @@
+﻿using Ploeh.AutoFixture;
+using Ploeh.AutoFixture.AutoNSubstitute;
+using Ploeh.AutoFixture.NUnit;
+
+namespace StringCalculator.UnitTests
+{
+    public class CalculatorTestConventionsAttribute : AutoTestCaseAttribute
+    {
+        public CalculatorTestConventionsAttribute() 
+            : base(new Fixture().Customize(new AutoNSubstituteCustomization()))
+        {
+        }
+    }
+}
