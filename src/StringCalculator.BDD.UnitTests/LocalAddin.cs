4affd8460d414d5199eab318f9acfac2dc6e8f49
Gert Jansen van Rensburg <gertjvr@gmail.com>
2013-09-27 16:49:54 +1000

    Trying something

--- src/StringCalculator.BDD.UnitTests/LocalAddin.cs
+++ src/StringCalculator.BDD.UnitTests/LocalAddin.cs
@@ -1,10 +1,21 @@
 ﻿using NUnit.Core.Extensibility;
 using Ploeh.AutoFixture.NUnit2.Addins;
+using Ploeh.AutoFixture.NUnit2.Addins.Builders;
 
 namespace StringCalculator.BDD.UnitTests
 {
     [NUnitAddin(Name = Constants.AutoDataExtension)]
-    public class LocalAddin : Ploeh.AutoFixture.NUnit2.Addins.Addin
+    public class LocalAddin : IAddin
     {
+        public bool Install(IExtensionHost host)
+        {
+            var providers = host.GetExtensionPoint("TestCaseProviders");
+            if (providers == null)
+                return false;
+
+            providers.Install(new AutoDataProvider());
+
+            return true;
+        }
     }
 }
