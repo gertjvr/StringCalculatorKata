1a3f50e8b09e5324aa85e78a71a56db32e26ed11
Gert Jansen van Rensburg <gertjvr@gmail.com>
2013-09-27 16:41:16 +1000

    Added LocalAddin to each test assembly

---
+++ src/StringCalculator.BDD.UnitTests/LocalAddin.cs
@@ -0,0 +1,10 @@
+﻿using NUnit.Core.Extensibility;
+using Ploeh.AutoFixture.NUnit2.Addins;
+
+namespace StringCalculator.BDD.UnitTests
+{
+    [NUnitAddin(Name = Constants.AutoDataExtension)]
+    public class LocalAddin : Ploeh.AutoFixture.NUnit2.Addins.Addin
+    {
+    }
+}
---
+++ src/StringCalculator.UnitTests/LocalAddin.cs
@@ -0,0 +1,10 @@
+﻿using NUnit.Core.Extensibility;
+using Ploeh.AutoFixture.NUnit2.Addins;
+
+namespace StringCalculator.UnitTests
+{
+    [NUnitAddin(Name = Constants.AutoDataExtension)]
+    public class LocalAddin : Ploeh.AutoFixture.NUnit2.Addins.Addin
+    {
+    }
+}
