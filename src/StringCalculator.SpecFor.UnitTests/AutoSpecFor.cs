2d39cd599df828aa5a8f71d527005e1d899815ed
Gert Jansen van Rensburg <gertjvr@gmail.com>
2013-10-03 00:30:04 +1000

    Added SpecificationFor Pattern UnitTests

---
+++ src/StringCalculator.SpecFor.UnitTests/AutoSpecFor.cs
@@ -0,0 +1,20 @@
+﻿using Ploeh.AutoFixture;
+
+namespace StringCalculator.SpecFor.UnitTests
+{
+    public abstract class AutoSpecFor<T> : SpecFor<T>
+    {
+        protected IFixture Fixture;
+
+        protected AutoSpecFor()
+            : this(new Fixture())
+        {
+   
+        }
+
+        protected AutoSpecFor(IFixture fixture)
+        {
+            Fixture = fixture;
+        }
+    }
+}
