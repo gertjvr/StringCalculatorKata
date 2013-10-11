94acd08cf2db88800b6a5442f08eb370999ef249
Gert Jansen van Rensburg <gertjvr@gmail.com>
2013-10-11 14:42:34 +1000

    Added StringCalculator.SpecFor.Xunit.UnitTests

---
+++ src/StringCalculator.SpecFor.Xunit.UnitTests/SpecFor.cs
@@ -0,0 +1,40 @@
+﻿using Xunit;
+
+namespace StringCalculator.SpecFor.Xunit.UnitTests
+{
+    public abstract class SpecFor<T>
+    {
+        private bool _initialized;
+
+        protected T Subject;
+
+        protected abstract T Given();
+
+        protected abstract void When();
+
+        public void Run()
+        {
+            if (_initialized != false) 
+                return;
+
+            Subject = Given();
+            When();
+
+            _initialized = true;
+        }
+
+        protected void CheckExists(object value)
+        {
+            Assert.NotNull(value);
+        }
+
+        protected void CheckValue<TValue>(TValue expectedValue, TValue actualValue)
+        {
+            Assert.Equal(expectedValue, actualValue);
+        }
+    }
+
+    public class ThenAttribute : FactAttribute
+    {   
+    }
+}
