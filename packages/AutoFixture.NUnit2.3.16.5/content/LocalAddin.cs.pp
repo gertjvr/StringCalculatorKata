180a9efb3851d47d6e0fc15835375d488c0b9da0
gertjvr <gertjvr@gmail.com>
2014-02-22 19:27:54 +1000

    Upgraded AutoFixture to latest version.

--- packages/AutoFixture.NUnit2.3.16.5/content/LocalAddin.cs.pp
+++ packages/AutoFixture.NUnit2.3.16.5/content/LocalAddin.cs.pp
@@ -1,6 +1,6 @@
 using NUnit.Core.Extensibility;
 
-namespace StringCalculator.NUnit.UnitTests
+namespace $rootnamespace$
 {
     [NUnitAddin]
     public class LocalAddin : Ploeh.AutoFixture.NUnit2.Addins.Addin
